7aa9c7c2231c38f0634a43e8800e003ea2f6c88c
Miriam-Pinto <101152717+Miriam-Pinto@users.noreply.github.com>
2024-01-28 15:42:07 +0000

    Revert "a"
    This reverts commit 1bbfa5c8d2db925a229cf741113d0a04bf2e1191.

--- PechinchaMarket/PechinchaMarket/obj/Debug/net8.0/PechinchaMarket.AssemblyInfo.cs
+++ PechinchaMarket/PechinchaMarket/obj/Debug/net8.0/PechinchaMarket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PechinchaMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bb659053db75474635dee25bfe83913ca35634e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PechinchaMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PechinchaMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
