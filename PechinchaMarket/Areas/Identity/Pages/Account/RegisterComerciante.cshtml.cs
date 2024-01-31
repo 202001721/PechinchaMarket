2bead0d2c1cbee3db1d92df4fc02fc1795b3fbe1
MatildePereira <202100343@estudantes.ips.pt>
2024-01-31 15:42:03 +0000

    botão de cancelar registo

--- PechinchaMarket/Areas/Identity/Pages/Account/RegisterComerciante.cshtml.cs
+++ PechinchaMarket/Areas/Identity/Pages/Account/RegisterComerciante.cshtml.cs
@@ -50,6 +50,11 @@ namespace PechinchaMarket.Areas.Identity.Pages.Account
             _environment = environment;
         }
 
+        public IActionResult Back()
+        {
+            return new RedirectToPageResult("~/Identity/Account/Register");
+        }
+
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
