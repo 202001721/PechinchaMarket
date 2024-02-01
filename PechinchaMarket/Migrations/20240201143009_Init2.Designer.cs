d5685d19b50acd62c0faa5ab2f6b375be0933e74
LuisFonseca-cyber <80035086+LuisFonseca-cyber@users.noreply.github.com>
2024-02-01 15:00:44 +0000

    Sign up google done

--- PechinchaMarket/Migrations/20240201143009_Init2.Designer.cs
+++ PechinchaMarket/Migrations/20240201143009_Init2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PechinchaMarket.Areas.Identity.Data;
 
@@ -11,9 +12,11 @@ using PechinchaMarket.Areas.Identity.Data;
 namespace PechinchaMarket.Migrations
 {
     [DbContext(typeof(DBPechinchaMarketContext))]
-    partial class DBPechinchaMarketContextModelSnapshot : ModelSnapshot
+    [Migration("20240201143009_Init2")]
+    partial class Init2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
