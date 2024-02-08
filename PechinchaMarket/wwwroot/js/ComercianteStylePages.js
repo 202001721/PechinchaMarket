12b2e4d252d377cfe4e4ef7b0450ab9a82e62f70
Miriam-Pinto <101152717+Miriam-Pinto@users.noreply.github.com>
2024-02-08 19:30:55 +0000

    Revert "style"
    This reverts commit e9a80cae06b6210e181c13fc6dd150ad3e93f5d8.

--- PechinchaMarket/wwwroot/js/ComercianteStylePages.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the root element
-    var root = document.documentElement;
-
-    // Set the value of the CSS variable to blue
-    root.style.setProperty('--root-main-color', rgb(102, 209, 255));
-});
