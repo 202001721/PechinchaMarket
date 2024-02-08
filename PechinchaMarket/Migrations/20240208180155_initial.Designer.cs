ce9edb0f0b51b4f74620ac6886365ba1163d58f2
MatildePereira <202100343@estudantes.ips.pt>
2024-02-08 18:24:33 +0000

    alguns arranjos

--- PechinchaMarket/Migrations/20240208180155_initial.Designer.cs
+++ PechinchaMarket/Migrations/20240208180155_initial.Designer.cs
@@ -12,8 +12,8 @@ using PechinchaMarket.Areas.Identity.Data;
 namespace PechinchaMarket.Migrations
 {
     [DbContext(typeof(DBPechinchaMarketContext))]
-    [Migration("20240207212421_Initial")]
-    partial class Initial
+    [Migration("20240208180155_initial")]
+    partial class initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
