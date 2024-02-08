e9a80cae06b6210e181c13fc6dd150ad3e93f5d8
Miriam-Pinto <101152717+Miriam-Pinto@users.noreply.github.com>
2024-02-08 19:28:36 +0000

    style

---
+++ PechinchaMarket/wwwroot/js/ComercianteStylePages.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the root element
+    var root = document.documentElement;
+
+    // Set the value of the CSS variable to blue
+    root.style.setProperty('--root-main-color', rgb(102, 209, 255));
+});
