0fa113f5a48e7c5b9f5c173eff1205021b07727c
LuisFonseca-cyber <80035086+LuisFonseca-cyber@users.noreply.github.com>
2024-02-08 17:11:25 +0000

    Testes Incompleto

---
+++ PechinchaMarketTest/ApplicationDbContextFixture.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using PechinchaMarket.Areas.Identity.Data;
+using PechinchaMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PechinchaMarketTest
+{
+    public class ApplicationDbContextFixture : IDisposable
+    {
+        public DBPechinchaMarketContext DbContext { get; private set; }
+
+        public ApplicationDbContextFixture() {
+            
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<DBPechinchaMarketContext>()
+                    .UseSqlite("DataSource=:memory:")
+                    .Options;
+            DbContext = new DBPechinchaMarketContext(options);
+            DbContext.Database.EnsureCreated();
+
+            // Adicione uma nova liga para teste
+            var user = CreateUser();
+            
+            DbContext.Cliente.Add(new Cliente { Id=Guid.NewGuid(), UserId="", Localizacao="Lisboa", Preferencias=new List<Categoria>() });
+
+            DbContext.SaveChanges();
+        }
+
+        public void Dispose() => DbContext.Dispose();
+    }
+}
---
+++ PechinchaMarketTest/ClientesControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PechinchaMarket.Areas.Identity.Data;
+using PechinchaMarket.Controllers;
+
+namespace PechinchaMarketTest
+{
+    public class ClientesControllerTest : IClassFixture<ApplicationDbContextFixture>
+    {
+
+        private DBPechinchaMarketContext _context;
+
+        public ClientesControllerTest(ApplicationDbContextFixture context)
+        {
+            _context = context.DbContext;
+        }
+
+    }
+}
