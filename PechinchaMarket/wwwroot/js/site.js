018d578faec927b319734e19d42595b2aade2a38
LuisFonseca-cyber <80035086+LuisFonseca-cyber@users.noreply.github.com>
2024-02-09 16:25:10 +0000

    Update

--- PechinchaMarket/wwwroot/js/site.js
+++ PechinchaMarket/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
+﻿/**
+ *  This event is so all authentication inputs text heigth is adjusted ignoring a size of 24 px;
+ */
+document.addEventListener('DOMContentLoaded', function () {
     var elements = document.querySelectorAll('.authentication-input-text-div');
     elements.forEach(function (element) {
         var height = element.getBoundingClientRect().height;
@@ -6,6 +9,12 @@
     });
 });
 
+/**
+ * This function will update the current url with a prefix key and value.
+ * 
+ * @param {any} key the key name in the url
+ * @param {any} value the value of that key in the url
+ */
 function updateUrlParameter(key, value) {
     const urlParams = new URLSearchParams(window.location.search);
     urlParams.set(key, value);
@@ -13,17 +22,22 @@ function updateUrlParameter(key, value) {
     window.history.replaceState(null, null, newUrl);
 }
 
+/**
+ * This function will update a label that corresponds to the uploaded file name on the basis that its a uncle of the input type file
+ * 
+ * @param input the input type file 
+ */
 function updateFileName(input) {
     var fileContainer = input.parentNode.parentNode; // Get the parent node of the input (the .insert-file container)
     var fileNameElement = fileContainer.querySelector('.file-name');
-    fileNameElement.innerText = input.files[0] ? input.files[0].name : 'No file chosen';
+    fileNameElement.innerText = input.files[0] ? input.files[0].name : 'Nenhum ficheiro escolhido';
 }
 
 /**
  * This function toggles the input-text type between 'password' and 'text', allowing the password to be displayed to the user.
  * (This function assumes that the button to hide or to show the password is a sibling of the input-text)
  * 
- * @param element correspondes to the button that hiddes or makes the password vissiable 
+ * @param element the button that hiddes or makes the password vissiable 
  */
 function togglePassword(element) {
     var passwordContainer = element.parentNode;
@@ -48,6 +62,7 @@ function togglePassword(element) {
 function adjustTogglePassword(element) {
     var passwordInput = element.querySelector('.password-hidden, .password-visible');
     var dangerChild = element.querySelector('.text-danger');
+    var inputElement = document.querySelector('input[type="text"]');
     if (passwordInput && dangerChild) {
         var initialHeight = dangerChild.getBoundingClientRect().height;
         var currentBottom = parseInt(getComputedStyle(passwordInput).bottom);
@@ -58,6 +73,23 @@ function adjustTogglePassword(element) {
                 if (currentHeight !== initialHeight) {
                     var dangerChildHeight = currentHeight;
                     passwordInput.style.bottom = (currentBottom + dangerChildHeight) + 'px';
+                    initialHeight = currentHeight;
+                }
+            });
+        });
+
+        observer.observe(dangerChild, { attributes: true });
+    }
+
+    if (inputElement && dangerChild) {
+        var initialHeight = dangerChild.getBoundingClientRect().height;
+        var inputHeight = 40;
+        var observer = new MutationObserver(function (mutationsList) {
+            mutationsList.forEach(function (mutation) {
+                var currentHeight = dangerChild.getBoundingClientRect().height;
+                if (currentHeight !== initialHeight) {
+                    var dangerChildHeight = currentHeight;
+                    element.style.height = (inputHeight + dangerChildHeight) + 'px';
                     initialHeight = currentHeight;
                 }
             });
@@ -76,6 +108,9 @@ function observeTextDanger() {
 
 document.addEventListener('DOMContentLoaded', observeTextDanger);
 
+/**
+ *  This function will change the current window to the previous opened on the history
+ */
 function goBack() {
     window.history.back();
 }
