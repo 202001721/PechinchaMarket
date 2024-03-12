ee9c13f3fe0dfa2cd53dc3dd031b1e9a4b677adf
MatildePereira <202100343@estudantes.ips.pt>
2024-03-12 15:30:19 +0000

    documentação

---
+++ html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['brand_0',['Brand',['../class_pechincha_market_1_1_models_1_1_produto.html#ae1d4935a65a0227db6170c25dbb12625',1,'PechinchaMarket::Models::Produto']]],
+  ['buildtargetmodel_1',['BuildTargetModel',['../class_pechincha_market_1_1_migrations_1_1_init.html#a07c5ffda99355511e6c17314aed2ffc1',1,'PechinchaMarket::Migrations::Init']]]
+];
---
+++ html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['buildtargetmodel_0',['BuildTargetModel',['../class_pechincha_market_1_1_migrations_1_1_init.html#a07c5ffda99355511e6c17314aed2ffc1',1,'PechinchaMarket::Migrations::Init']]]
+];
