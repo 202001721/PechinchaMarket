94141aebe37fb5aadaef43acbeaa187f4afcccef
Bruno Machado <202001721@estudantes.ips.pt>
2024-03-13 21:55:51 +0000

    enviando email com a API

--- PechinchaMarket/Areas/Identity/Pages/Account/RegisterCliente.cshtml.cs
+++ PechinchaMarket/Areas/Identity/Pages/Account/RegisterCliente.cshtml.cs
@@ -24,6 +24,7 @@ using Microsoft.Extensions.Logging;
 using PechinchaMarket.Areas.Identity.Data;
 using PechinchaMarket.Models;
 using Microsoft.AspNet.Identity;
+using PechinchaMarket.Services;
 
 namespace PechinchaMarket.Areas.Identity.Pages.Account
 {
@@ -34,7 +35,7 @@ namespace PechinchaMarket.Areas.Identity.Pages.Account
         private readonly Microsoft.AspNetCore.Identity.IUserStore<PechinchaMarketUser> _userStore;
         private readonly Microsoft.AspNetCore.Identity.IUserEmailStore<PechinchaMarketUser> _emailStore;
         private readonly ILogger<RegisterClienteModel> _logger;
-        private readonly IEmailSender _emailSender;
+        // private readonly IEmailSender _emailSender;
         private readonly DBPechinchaMarketContext _context;
 
 
@@ -51,7 +52,7 @@ namespace PechinchaMarket.Areas.Identity.Pages.Account
             _emailStore = (Microsoft.AspNetCore.Identity.IUserEmailStore<PechinchaMarketUser>)GetEmailStore();
             _signInManager = signInManager;
             _logger = logger;
-            _emailSender = emailSender;
+            //_emailSender = emailSender;
             _context = context;
         }
 
@@ -168,13 +169,16 @@ namespace PechinchaMarket.Areas.Identity.Pages.Account
                     var callbackUrl = Url.Page(
                         "/Account/ConfirmEmail",
                         pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl, context = _context },
                         protocol: Request.Scheme);
 
+                    EmailSender emailSenderService = new EmailSender(_context);
 
+                    emailSenderService.SendEmail("Confirme seu email",Input.Email,Input.UserName,
+                        $"Por favor confirme o seu registo no PechinchaMarket {HtmlEncoder.Default.Encode(callbackUrl)}").Wait();
 
-                    await SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    /* await SendEmailAsync(Input.Email, "Confirm your email",
+                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");*/
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
@@ -205,6 +209,9 @@ namespace PechinchaMarket.Areas.Identity.Pages.Account
         /// <param name="email"></param> email do utilizador
         /// <param name="subject"></param> assunto do email
         /// <param name="confirmLink"></param> mensagem com o link de confirmação
+        
+
+        /*
         private async Task<bool> SendEmailAsync(string email, string subject, string confirmLink)
         {
             try
@@ -234,7 +241,7 @@ namespace PechinchaMarket.Areas.Identity.Pages.Account
             {
                 return false;
             }
-        }
+        }*/
 
         private PechinchaMarketUser CreateUser()
         {
---
+++ PechinchaMarket/Services/EmailSender.cs
@@ -0,0 +1,30 @@
+﻿using SendGrid.Helpers.Mail;
+using SendGrid;
+using PechinchaMarket.Areas.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace PechinchaMarket.Services
+{
+    public class EmailSender
+    {
+        private DBPechinchaMarketContext _context;
+        public EmailSender(DBPechinchaMarketContext context) {
+            _context = context;
+        }
+        public async Task SendEmail(string subject, string toEmail, string username,string message )
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("pechinchamarket@outlook.com", "PechinchaMarket");
+
+           
+           
+
+            var to = new EmailAddress(toEmail,username);
+            var plainTextContent = message;
+            var htmlContent = "";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
