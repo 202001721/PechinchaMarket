41f23efe3e91c61548baabff056727f90216ecf6
Bruno Machado <202001721@estudantes.ips.pt>
2024-03-17 13:31:06 +0000

    erro da key arranjado

--- PechinchaMarket/Services/EmailSender.cs
+++ PechinchaMarket/Services/EmailSender.cs
@@ -14,7 +14,7 @@ namespace PechinchaMarket.Services
         }
         public async Task SendEmail(string subject, string toEmail, string username,string message )
         {
-            var apiKey = _config["SendGrid:ApiKey"];
+            var apiKey = "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";
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress("pechinchamarket@outlook.com", "PechinchaMarket");
 
