3c05b63069a5e41991493ed8ad7ee1b7588ecf08
LuisFonseca-cyber <80035086+LuisFonseca-cyber@users.noreply.github.com>
2024-03-30 19:51:57 +0000

    Login/Registro ajustados e melhorados

--- PechinchaMarket/wwwroot/js/AuthenticationBasedOnPhases.js
+++ PechinchaMarket/wwwroot/js/AuthenticationBasedOnPhases.js
@@ -12,9 +12,20 @@ document.addEventListener("DOMContentLoaded", function () {
 function toogleSignUpFase(index, phaseslength) {
     for (let i = 0; i < phaseslength; i++) {
         document.getElementById('phase' + i).classList.add('display-none');
+        var phasetext = document.getElementById('text-phase' + i);
+        if (phasetext) {
+            if (!phasetext.classList.contains('display-none')) {
+                phasetext.classList.add('display-none');
+            }            
+        }
     }
 
     document.getElementById('phase' + index).classList.remove('display-none');
+    var phasetexti = document.getElementById('text-phase' + index);
+    if (phasetexti) {
+        phasetexti.classList.remove('display-none');
+    }
+
 
     updateUrlParameter('phase', index);
 }
