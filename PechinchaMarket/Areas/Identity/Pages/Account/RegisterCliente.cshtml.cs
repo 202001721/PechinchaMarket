2fcb103efc4d9453f25708e7199bd007731402fe
Miriam-Pinto <101152717+Miriam-Pinto@users.noreply.github.com>
2024-04-28 18:43:07 +0100

    erros em pt

--- PechinchaMarket/Areas/Identity/Pages/Account/RegisterCliente.cshtml.cs
+++ PechinchaMarket/Areas/Identity/Pages/Account/RegisterCliente.cshtml.cs
@@ -111,7 +111,7 @@ namespace PechinchaMarket.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "A password não é válida", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
@@ -122,7 +122,7 @@ namespace PechinchaMarket.Areas.Identity.Pages.Account
             /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Compare("Password", ErrorMessage = "A password e a confirmação da password não são a mesma.")]
             public string ConfirmPassword { get; set; }
         }
 
